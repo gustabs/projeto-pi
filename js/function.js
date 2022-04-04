a4980e2c218daa64e739ce750df9210252044610
Gustavo <gugmoises@gmail.com>
2022-04-04 17:42:17 -0300

    up

---
+++ js/function.js
@@ -0,0 +1,12 @@
+function validar(){
+    if (document.formu.nome.value == "" || document.formu.nome.value. length <3){
+        alert('Preencha o campo NOME')
+        return false;
+    }
+    if(document.formu.email.value == "" || document.formu.email.value.indexOf('@')== -1) {
+        alert('Preencha o campo E-mail correspondente')
+        return false;
+    }
+
+    if
+}

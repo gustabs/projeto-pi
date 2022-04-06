744e992d159bfae9f12ffe97c2a480999126baf8
Gustavo <gugmoises@gmail.com>
2022-04-06 16:58:01 -0300

    projeto pré pronto

--- js/script.js
+++ js/script.js
@@ -1,21 +1,29 @@
 function validar(){
-    if (document.formu.nome.value == "" || document.formu.nome.value. length <3){
-        alert('Preencha o campo NOME')
+    if(document.dados.lNome.value == ""){
+        alert('Preencha o campo Nome.')
         return false;
     }
-
-    if (document.formu.email.value == "" || document.formu.email.value.indexOf('@')== -1){
-        alert('Preencha o campo E-mail corretamente')
+    if(document.dados.lSenha.value == "") {
+        alert('Preencha o campo Senha.')
         return false;
     }
-
-    if (document.formu.email.value.indexOf('.') == -1){
-        alert('E-mail digitado de forma incorreta')
+    if(document.dados.lSenha.value.length <=8){
+        alert('O campo Senha deve conter mais que 8 caracteres!')
         return false;
     }
-
-    if (document.formumensagem.value == "") {
-        alert('Preencha algo em mensagens')
+    if(document.dados.lEmail.value == "") {
+        alert('Preencha o campo Email.')
         return false;
+    }
+    if(document.dados.lEmail.value.indexOf('@')== -1){
+        alert('Preencha o campo Email corretamente contendo "@".')
+        return false;
+    }
+    if(document.dados.lEmail.value.indexOf('.')== -1) {
+        alert('Preencha o campo Email corretamente contendo ".".')
+        return false;
+    }else{
+        alert('Cadastro criado com sucesso.')
+        return true;
     }
 }

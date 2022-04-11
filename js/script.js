9c66edd1739c6e32c292027aa9a3d8a8a8100f3b
Gustavo <gugmoises@gmail.com>
2022-04-11 17:46:26 -0300

    up

--- js/script.js
+++ js/script.js
@@ -1,29 +1,29 @@
-function validar(){
-    if(document.dados.lNome.value == ""){
-        alert('Preencha o campo Nome.')
-        return false;
+    function validar(){
+        if(document.dados.lNome.value == ""){
+            alert('Preencha o campo Nome.')
+            return false;
+        }
+        if(document.dados.lSenha.value == "") {
+            alert('Preencha o campo Senha.')
+            return false;
+        }
+        if(document.dados.lSenha.value.length <=8){
+            alert('O campo Senha deve conter mais que 8 caracteres!')
+            return false;
+        }
+        if(document.dados.lEmail.value == "") {
+            alert('Preencha o campo Email.')
+            return false;
+        }
+        if(document.dados.lEmail.value.indexOf('@')== -1){
+            alert('Preencha o campo Email corretamente contendo "@".')
+            return false;
+        }
+        if(document.dados.lEmail.value.indexOf('.')== -1) {
+            alert('Preencha o campo Email corretamente contendo ".".')
+            return false;
+        }else{
+            alert('Cadastro criado com sucesso.')
+            return true;
+        }
     }
-    if(document.dados.lSenha.value == "") {
-        alert('Preencha o campo Senha.')
-        return false;
-    }
-    if(document.dados.lSenha.value.length <=8){
-        alert('O campo Senha deve conter mais que 8 caracteres!')
-        return false;
-    }
-    if(document.dados.lEmail.value == "") {
-        alert('Preencha o campo Email.')
-        return false;
-    }
-    if(document.dados.lEmail.value.indexOf('@')== -1){
-        alert('Preencha o campo Email corretamente contendo "@".')
-        return false;
-    }
-    if(document.dados.lEmail.value.indexOf('.')== -1) {
-        alert('Preencha o campo Email corretamente contendo ".".')
-        return false;
-    }else{
-        alert('Cadastro criado com sucesso.')
-        return true;
-    }
-}

cf1d3913eb734ba0cd8505af2ccdb5d57f3cd529
Guxta <gugmoises@gmail.com>
2022-06-20 17:45:12 -0300

    up

--- js/formv.js
+++ js/formv.js
@@ -1,29 +1,35 @@
-    function formv(){
-    if(document.dados.lNome.value == ""){
-        alert('Preencha o campo Nome.')
-        return false;
+    function formv() {
+        if (document.dados.lNome.value == "") {
+            alert('Preencha o campo Nome.')
+            return false;
+        }
+
+        if (document.dados.lSenha.value == "") {
+            alert('Preencha o campo Senha.')
+            return false;
+        }
+        if (document.dados.lSenha.value.length <= 8) {
+            alert('O campo Senha deve conter mais que 8 caracteres!')
+            return false;
+            //este script é para caso o campo email nao tenha sido preemchido
+        }
+        if (document.dados.lEmail.value == "") {
+            alert('Preencha o campo Email.')
+            return false;
+            //este script é para caso o campo email nao tenha sido preenchido
+        }
+        if (document.dados.lEmail.value.indexOf('@') == -1) {
+            alert('Preencha o campo Email corretamente contendo "@".')
+            return false;
+            //este script é para caso nao tenha "@" no campo email
+        }
+        if (document.dados.lEmail.value.indexOf('.') == -1) {
+            alert('Preencha o campo Email corretamente contendo ".".')
+            return false;
+            //este script é para caso nao tenha "." no campo email
+        } else {
+            alert('Cadastro criado com sucesso.')
+            return true;
+            //se for feito tudo de forma correta vai para o else
+        }
     }
-    if(document.dados.lSenha.value == "") {
-        alert('Preencha o campo Senha.')
-        return false;
-    }
-    if(document.dados.lSenha.value.length <=8){
-        alert('O campo Senha deve conter mais que 8 caracteres!')
-        return false;
-    }
-    if(document.dados.lEmail.value == "") {
-        alert('Preencha o campo Email.')
-        return false;
-    }
-    if(document.dados.lEmail.value.indexOf('@')== -1){
-        alert('Preencha o campo Email corretamente contendo "@".')
-        return false;
-    }
-    if(document.dados.lEmail.value.indexOf('.')== -1) {
-        alert('Preencha o campo Email corretamente contendo ".".')
-        return false;
-    }else{
-        alert('Cadastro criado com sucesso.')
-        return true;
-    }
-}

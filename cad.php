42d0dae7772b6027ec0f9085b8b8aea2d8036014
Guxta <gugmoises@gmail.com>
2022-07-04 15:15:23 -0300

    up

--- cad.php
+++ cad.php
@@ -4,13 +4,13 @@ $nome = $_POST ['lNome'];
 $usuario = $_POST ['lEmail'];
 $senha = $_POST ['lSenha'];
 
-$sql= "INSERT INTO cadastro
-(nome, usuario, senha) VALUES
+$sql= "INSERT INTO usuario
+(Nome, usuario, senha) VALUES
 ('$nome', '$usuario', '$senha')";
 $query= mysqli_query($conexao, $sql);
 ?>
 
 <script>
     alert('Sua compra foi efetuada com sucesso')
-    window.location.href="log.html"
+    window.location.href="index.php"
 </script>
